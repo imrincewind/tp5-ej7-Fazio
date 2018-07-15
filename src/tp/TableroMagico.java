7b79c1f51c86a0c9c071e50f9fc938c9f3f4c1d6
Ezequiel Fazio <ezfazio@gmail.com>
2018-07-15 14:37:22 -0300

    final commit

--- src/tp/TableroMagico.java
+++ src/tp/TableroMagico.java
@@ -3,78 +3,71 @@ package tp;
 
 
 public class TableroMagico {
-	private static final int S = 2;
-	private static final int K = 2;
-	private static final int N = 2;
+	private static final int S = 15;//23
+	private static final int K = 9;//13
+	private static final int N = 3;//3
 
 	public static void main(String[] args) {
 		int[][] tablero = new int[N][N];
-		
+	
 		
 		tablero = resolver(tablero);
-		
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				System.out.print(tablero[i][j] + "-");
 			}
 			System.out.println();
 		}
-		
+	
+
 	}
 
 	public static int[][] resolver(int[][] tablero) {
-		
-		tablero = backMagico(tablero, 0, 0, 1);
+		int[] candidatos = new int[K]; //para no repetir números y mejorar tiempos de ejecución
+		for (int i = 0; i < K; i++) {
+				candidatos[i] = i+1;	
+		}
+		tablero = backMagico(tablero,candidatos, 0);
 
 		return tablero;
 	}
 
-	private static int[][] backMagico(int[][] tablero, int nivel, int nivelI, int j) {
-		if (nivel == N - 1 && nivelI == N -1 ) {
-			if (esSolucion(tablero)) {
-				return tablero;
-			}
-		} else {
-			for (int i = 0; i < N; i++) {
-				while (j < K && nivel < N) {
-					tablero[nivel][i] = j;
-					int[][] sol = backMagico(tablero, nivel+1,i , j + 1);
-					if (i == N - 1 && nivel < N) {
-						//i = 0;
-						sol = backMagico(tablero, nivel + 1,i, j + 1);
+	private static int[][] backMagico(int[][] tablero, int[] candidatos, int nivel) {
+		if (nivel == (N * N) - 1 && esSolucion(tablero)) {
+
+			return tablero;
+
+		} else if(nivel < (N * N)){
+			
+				for (int i = 0; i < K; i++) {
+					
+					int temp = 0;
+					if(candidatos[i] != 0){
+						temp = candidatos[i];
+						candidatos[i] = 0;
+					tablero[nivel / N][nivel % N] = temp;
+					
 					}
+					int[][] sol = backMagico(tablero,candidatos, nivel + 1);
+					candidatos[i] = temp;
 					if (sol != null) {
 						return sol;
 					}
 				}
 			}
-
-		}
+	
+		
 
 		return null;
 	}
 
 	private static boolean esSolucion(int[][] tablero) {
-		return numerosDistintos(tablero) && sumas(tablero);
+		return sumas(tablero);
 	}
 
-	private static boolean numerosDistintos(int[][] tablero) {
-		int temp = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				temp = tablero[i][j];
-				for (int g = i; i < N; g++) {
-					for (int h = j + 1; j < N; h++) {
-						if (temp == tablero[g][h]) {
-							return false;
-						}
-					}
+	
 
-				}
-			}
-		}
-		return true;
-	}
 
 	private static boolean sumas(int[][] tablero) {
 
